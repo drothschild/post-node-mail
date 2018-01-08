35af99100a40e5fb7ed218607bac984a7730d92c
drothschild <david@rothschildsolutions.com>
2018-01-07 19:52:17 -0800

    app recognizes json body and takes variables from it \

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,8 +7,8 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.post('*', (req, res) => {
-    console.log(req);
- res.send("we are up for posting");
+    const {to, subject, body} = req.body;
+    res.send(`Email with subject: '${subject}', body: ${body}, sent to ${to}`)
 });
 
 app.listen(port);
